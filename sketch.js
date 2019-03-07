39dc6ba48fbb457f324524779e577989d7547e95
Maël Acier <mael.acier@pm.me>
2019-03-07 23:38:29 +0100

    coord.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
+var p;
+
 function setup() {
   createCanvas(400, 400, WEBGL);
+  p = createElement('p',"Z:0"+" X:0"+" Y:0")
 }
 
 function draw() {
@@ -7,5 +10,6 @@ function draw() {
   rotateZ(radians(rotationZ));
   rotateX(radians(rotationX));
   rotateY(radians(rotationY));
+  p.html("Z:"+rotationZ+" X:"+ rotationX+" Y:"+rotationY)
   box(200, 200, 200);
 }
