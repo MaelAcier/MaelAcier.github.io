7a7db2ed0cf89fcd0f0c5e69da1a1fd2de3f1e67
Maël Acier <mael.acier@pm.me>
2019-03-07 23:17:02 +0100

    orientation v2 p5.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 function setup() {
-  createCanvas(100, 100, WEBGL);
+  createCanvas(400, 400, WEBGL);
 }
 
 function draw() {
   background(200);
   //rotateZ(radians(rotationZ));
   rotateX(radians(rotationX));
+  rotateY(radians(rotationY));
+  rotateZ(radians(rotationZ));
   //rotateY(radians(rotationY));
   box(200, 200, 200);
 }
